5b983b98215a866c6186138bee9293140fc61b79
gummisig94 <gudmundurs@hotmail.com>
2019-10-06 18:21:06 +0000

    node js, reyna að setja upp server - virkar ekki alveg er að vinna í að laga það

--- server.js
+++ server.js
@@ -1,8 +1,20 @@
 'use strict';
 var http = require('http');
+var hostname = '127.0.0.1';
 var port = process.env.PORT || 1337;
 
 http.createServer(function (req, res) {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.end('Hello World\n');
-}).listen(port);
+server.get("/", (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+}).listen(port, hostname, function(){
+    console.log('Server running at http://'+ hostname + ':' + port + '/');
+});
+
+server.get("/folf", (req, res) => {
+    res.sendFile(__dirname + '/folf.html');
+ });
+
+// https://www.pluralsight.com/guides/getting-started-with-nodejs
